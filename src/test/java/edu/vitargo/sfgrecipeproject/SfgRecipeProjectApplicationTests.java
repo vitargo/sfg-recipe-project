e8c2cc8e6dfe14967f5eda7d1b413aeabb7aa907
viktoriia.redko <viktoriia.redko@ericsson.com>
2022-08-09 12:15:55 +0300

    init commit

--- src/test/java/edu/vitargo/sfgrecipeproject/SfgRecipeProjectApplicationTests.java
+++ src/test/java/edu/vitargo/sfgrecipeproject/SfgRecipeProjectApplicationTests.java
@@ -1,13 +1,13 @@
-package edu.vitargo.sfgrecipeproject;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SfgRecipeProjectApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package edu.vitargo.sfgrecipeproject;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SfgRecipeProjectApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
